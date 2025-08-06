555d7c446913d5e47bd7823f0dde62783422da65
I Dewa Gde Hari Sudewa <dewagde1704@gmail.com>
2025-08-06 13:00:08 +0700

    .

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+export default {
+  darkMode: false,
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
